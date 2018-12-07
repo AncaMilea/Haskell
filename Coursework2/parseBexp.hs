f59471bbe4951a3236ac1e855fa308a3dc3e5b6e
Anca Elena <aem1u17@soton.ac.uk>
2018-12-07 21:06:31 +0000

    make it clear

--- Coursework2/parseBexp.hs
+++ Coursework2/parseBexp.hs
@@ -3,83 +3,59 @@ import Parsing
 
 data Expr = App Expr Expr | Let [Int] Expr Expr | Var Int deriving (Show,Eq)
 
-check :: Maybe a -> a
-check Nothing = error ("No binding found for variable ")
-check (Just b) = b
-
--- evaluation function to provide semantics to formulas
--- eval :: Substitution -> Expr -> Bool
--- eval _ Tru = True
--- eval _ Fls = False
--- eval s (Var p) = check $ lookup p s
--- eval s (And e1 e2) = (eval s e1) && (eval s e2)
--- eval s (Or e1 e2)  = (eval s e1) || (eval s e2)
-
 -- Parsing Code
+varExpression :: Parser Expr
+varExpression = do
+  symbol "x"
+  i <- nat
+  return (Var i)
 
-varsExp :: Parser [Int]
-varsExp = do
+
+letVariables :: Parser [Int]
+letVariables = do
   symbol "x"
   x <- nat
   xs <- many (do 
     symbol "x"
     nat)
   return (x:xs)
--- Expressions
--- Define a parser for each kind of expression
--- We use "ident" and "symbol" from Parsing.hs
-varExp :: Parser Expr
-varExp = do
-  symbol "x"
-  i <- nat
-  return (Var i)
 
-intExp :: Parser Int
-intExp = do s <- varExp
-            let i = retInt(s)
-            return (i)
 
-retInt (Var x) =x
+letExpression :: Parser Expr
+letExpression = do symbol "let"
+                   xs <- letVariables
+                   symbol "=" 
+                   e1 <- allExpressions
+                   symbol "in"
+                   e2 <- allExpressions
+                   return (Let xs e1 e2)
 
-letExp :: Parser Expr
-letExp = do symbol "let"
-            xs <- varsExp
-            symbol "=" 
-            e1 <- doAll
-            symbol "in"
-            e2 <- doAll
-            return (Let xs e1 e2)
+appExpression :: Parser Expr
+appExpression = do symbol "("
+                   e1 <- allExpressions
+                   symbol ")"
+                   return e1
 
-appExp :: Parser Expr
-appExp = do symbol "("
-            e1 <- doAll
-            symbol ")"
-            return e1
+inOrder :: [Expr] -> Expr
+inOrder (x:[])=x
+inOrder (x:y:xs)= foldl f (App x y) xs
+           where
+             f m n= App m n  
 
-doAll :: Parser Expr
-doAll = do 
-           xs<-many(expr)
-           return (recur xs)
-        where
-           recur (x:[])=x
-           recur (x:y:xs)= foldl f (App x y) xs
-           f x y= App x y            
+allExpressions :: Parser Expr
+allExpressions = do 
+                 xs<-many(higherExpr)
+                 return (inOrder xs)        
 
 -- Define the top level entry point
 -- use choice <|> to write alternatives in the grammar
-expr :: Parser Expr
-expr = appExp <|> lowerExpr 
-lowerExpr = letExp <|> evenLowerExpr
-evenLowerExpr = varExp <|> empty
--- expr :: Parser Expr
--- expr = appExp 
--- lowerExpr = appExp <|> varExp <|> letExp
+higherExpr :: Parser Expr
+higherExpr = appExpression <|> lowerExpr 
+lowerExpr = letExpression <|> bottomExpr
+bottomExpr = varExpression <|> empty
 
--- Finally, define the actual function to parse input strings
--- NB, this definition is just for demonstration. It is not a good
--- definition and needs improving
 parseLet :: String -> Maybe Expr
-parseLet s =  case (parse doAll s) of
+parseLet s = case (parse allExpressions s) of
   [(n, [])] -> Just n
   _ -> Nothing
 
